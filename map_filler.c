c34f125fccc28a818cdff7080435c230dafe803e
Marco Rojouan <mrojouan@3D4.42angouleme.fr>
2026-02-14 16:58:18 +0100

    super genial ce commit

--- map_filler.c
+++ map_filler.c
@@ -6,12 +6,21 @@
 /*   By: mrojouan <mrojouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 14:11:21 by mrojouan          #+#    #+#             */
-/*   Updated: 2026/02/12 14:34:54 by mrojouan         ###   ########.fr       */
+/*   Updated: 2026/02/13 14:10:28 by mrojouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <so_long.h>
 #include "get_next_line/get_next_line.h"
+
+static void change_last_char(char *line)
+{
+	int len;
+
+	len = ft_strlen(line);
+	if (len > 0 && line[len - 1] == '\n')
+	 	line[len - 1] = '\0';
+}
 
 static void get_map_size(t_map *map)
 {
@@ -19,17 +28,21 @@ static void get_map_size(t_map *map)
 	int		tmp_map_fd;
 
 	tmp_map_fd = open("map.ber", O_RDONLY);
+	if (tmp_map_fd < 0)
+		handle_map_error(map);
 	line = get_next_line(tmp_map_fd);
 	if (!line)
 		handle_map_error(map);
-	map->width = ft_strnlen(line);
+	change_last_char(line);
+	map->width = ft_strlen(line);
 	while (line)
 	{	
+		map->height++;
 		free(line);
 		line = get_next_line(tmp_map_fd);
-		map->height++;
+		if (line)
+			change_last_char(line);
 	}
-	free(line);
 	close(tmp_map_fd);
 }
 
@@ -64,14 +77,13 @@ void map_filler(t_map *map, int map_fd)
 	get_map_size(map);
 	map->map = malloc(sizeof(char *) * (map->height + 1));
 	if (!map->map)
-		return ;
-	map->map[i] = get_next_line(map_fd);
-	if (!map->map[i])
 		handle_map_error(map);
-	i++;
 	while (i < map->height)
 	{
 		map->map[i] = get_next_line(map_fd);
+		if (!map->map[i])
+			handle_map_error(map);
+		change_last_char(map->map[i]);
 		i++;
 	}
 	map->map[i] = NULL;
--- verif.c
+++ verif.c
@@ -6,7 +6,7 @@
 /*   By: mrojouan <mrojouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 15:06:02 by mrojouan          #+#    #+#             */
-/*   Updated: 2026/02/12 14:24:33 by mrojouan         ###   ########.fr       */
+/*   Updated: 2026/02/13 14:09:55 by mrojouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ static int verif_lines_len(t_map *map)
 	int		first_line_size;
 	int		i;
 
-	first_line_size = ft_strnlen(map->map[0]);
+	first_line_size = ft_strlen(map->map[0]);
 	i = 1;
 	while (i < map->height)
 	{
-		line_size = ft_strnlen(map->map[i]);
+		line_size = ft_strlen(map->map[i]);
 		if (line_size != first_line_size)
 			return (0);
 		i++;
 	}
-	return (line_size); 
+	return (1); 
 }
 
 static int verif_top_bottom(t_map *map)
@@ -79,14 +79,14 @@ static int verif_typo(t_map *map)
 	char current;
 
 	i = 0;
-	while (map->map[i])
+	while (i < map->height)
 	{
 		j = 0;
 		while (map->map[i][j])
 		{
 			current = map->map[i][j];
 			if (current != 'C' && current != 'P' && current != 'E'
-					&& current != '1' && current != '0' && current != '\n')
+					&& current != '1' && current != '0')
 				return (0);
 			j++;
 		}
@@ -99,7 +99,7 @@ void verif_map(t_map *map)
 {
 	if (verif_lines_len(map) == 0 || map->width <= 3)
 		handle_map_error(map);
-	if (map->height >= 0 && map->height <= 3)
+	if (map->height <= 3)
 		handle_map_error(map);
 	if (verif_top_bottom(map) == 0)
 		handle_map_error(map);
